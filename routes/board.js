80213a381cc697d3aad1caf0ea6526dc2264561f
Wesley Millett <s539200@nwmissouri.edu>
2023-11-01 21:03:50 -0500

    Part 8 Complete

--- routes/board.js
+++ routes/board.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('board', { title: 'Board Display' });
+  res.render('board', { title: 'Board Display', query: req.query });
 });
 
 module.exports = router;
